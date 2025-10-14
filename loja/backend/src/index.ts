560a2f19bc8a7ff3a991f73c56e8a131cc8fad3a
Luiz Gama <luiz.gama@icomp.ufam.edu.br>
2025-10-14 00:07:24 -0400

    ajustes index e package.json

--- loja/backend/src/index.ts
+++ loja/backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import session from 'express-session';
+import { v4 as uuidv4 } from 'uuid';
 
 import createLangCookie from './middlewares/createLangCookie';
 import router from './router/router';
@@ -9,6 +11,15 @@ dotenv.config();
 
 const app = express();
 const PORT = Number(process.env.PORT) || 6677;
+
+app.use(session({
+  genid: (req) => {
+    return uuidv4(); // Use UUIDs for session IDs
+  },
+  secret: process.env.SESSION_SECRET || 'default_secret',
+  resave: true,
+  saveUninitialized: true,
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
